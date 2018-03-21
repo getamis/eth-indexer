752281a869e7412bc5287d1861da13ad36e7dc61
azurechen <azure517981@gmail.com>
2018-03-21 16:30:26 +0800

    indexer, cmd/indexer: Add a basic indexer to get the block by number.

--- indexer/indexer.go
+++ indexer/indexer.go
@@ -1,24 +1,45 @@
 package indexer
 
 import (
+	"context"
+	"math/big"
+
 	"github.com/getamis/sirius/log"
 	store "github.com/maichain/eth-indexer/store/block_header"
+
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/getamis/sirius/log"
 )
+
+var logger = log.New()
 
 type Indexer interface {
 	Start()
 }
 
-func NewIndexer(store store.Store) Indexer {
+func NewIndexer(client *ethclient.Client, store store.Store) Indexer {
 	return &indexer{
+		client,
 		store,
 	}
 }
 
 type indexer struct {
-	store store.Store
+	client *ethclient.Client
+	store  store.Store
 }
 
-func (i *indexer) Start() {
-	log.Info("Start Indexing")
+func (indexer *indexer) Start() error {
+	ctx := context.TODO()
+
+	start := big.NewInt(1)
+	end := big.NewInt(10)
+	for i := new(big.Int).Set(start); i.Cmp(end) <= 0; i.Add(i, big.NewInt(1)) {
+		block, err := indexer.client.BlockByNumber(ctx, i)
+		if err != nil {
+			return err
+		}
+		logger.Info(block.Hash().String())
+	}
+	return nil
 }
