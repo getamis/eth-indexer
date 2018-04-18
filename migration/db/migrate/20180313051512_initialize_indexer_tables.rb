91f19ff446cae2087c8ddcb7d9c1313ac051c863
kimmylin <30611210+kimmylin@users.noreply.github.com>
2018-04-18 21:19:34 +0800

    migration: update data types and size limit

--- migration/db/migrate/20180313051512_initialize_indexer_tables.rb
+++ migration/db/migrate/20180313051512_initialize_indexer_tables.rb
@@ -1,53 +1,51 @@
 class InitializeIndexerTables < ActiveRecord::Migration
   def change
     create_table :block_headers do |t|
-      t.string :hash, :limit => 64, :null => false
-      t.string :parent_hash, :limit => 64, :null => false
-      t.string :uncle_hash, :limit => 64, :null => false
-      t.string :coinbase, :limit => 40, :null => false
-      t.string :root, :limit => 64, :null => false
-      t.string :tx_hash, :limit => 64, :null => false
-      t.string :receipt_hash, :limit => 64, :null => false
-      t.binary :bloom
+      t.binary :hash, :limit => 32, :null => false
+      t.binary :parent_hash, :limit => 32, :null => false
+      t.binary :uncle_hash, :limit => 32, :null => false
+      t.binary :coinbase, :limit => 20, :null => false
+      t.binary :root, :limit => 32, :null => false
+      t.binary :tx_hash, :limit => 32, :null => false
+      t.binary :receipt_hash, :limit => 32, :null => false
       t.integer :difficulty, :limit => 8, :null => false
       t.integer :number, :limit => 8, :null => false
-      t.column :gas_limit, 'BIGINT UNSIGNED'
-      t.column :gas_used, 'BIGINT UNSIGNED'
-      t.column :time, 'BIGINT UNSIGNED'
-      t.binary :extra_data
-      t.string :mix_digest
-      t.binary :nonce
+      t.integer :gas_limit, :limit => 8, :null => false
+      t.integer :gas_used, :limit => 8, :null => false
+      t.integer :time, :limit => 8, :null => false
+      t.binary :extra_data, :limit => 1024
+      t.binary :mix_digest, :limit => 32, :null => false
+      t.binary :nonce, :limit => 8, :null => false
     end
+    add_index :block_headers, :hash, :unique => true
     add_index :block_headers, :number, :unique => true
 
     create_table :transactions do |t|
-      t.string :hash, :limit => 64
-      t.string :block_hash, :limit => 64
-      t.string :from, :limit => 40
-      t.string :to, :limit => 40
-      t.binary :nonce
-      t.integer :gas_price, :limit => 8
-      t.column :gas_limit, 'BIGINT UNSIGNED'
-      t.integer :amount, :limit => 8
-      t.binary :payload
-      t.integer :v, :limit => 8
-      t.integer :s, :limit => 8
-      t.integer :r, :limit => 8
+      t.binary :hash, :limit => 32, :null => false
+      t.binary :block_hash, :limit => 32, :null => false
+      t.binary :from, :limit => 20, :null => false
+      t.binary :to, :limit => 20
+      t.integer :nonce, :limit => 8, :null => false
+      t.string :gas_price, :limit => 32, :null => false
+      t.integer :gas_limit, :limit => 8, :null => false
+      t.string :amount, :limit => 32, :null => false
+      t.binary :payload, :limit => 1.megabyte, :null => false
     end
     add_index :transactions, :hash, :unique => true
+    add_index :transactions, :block_hash
 
     create_table :transaction_receipts do |t|
-      t.binary :root, :limit => 64
-      t.column :status, 'INT UNSIGNED'
-      t.column :cumulative_gas_used, 'BIGINT UNSIGNED'
-      t.binary :bloom
-      t.string :tx_hash, :limit => 64
-      t.string :contract_address, :limit => 40
-      t.column :gas_used, 'BIGINT UNSIGNED'
+      t.binary :root, :limit => 32
+      t.integer :status, :limit => 1
+      t.integer :cumulative_gas_used, :limit => 8, :null => false
+      t.binary :bloom, :limit => 256, :null => false
+      t.binary :tx_hash, :limit => 32, :null => false
+      t.binary :contract_address, :limit => 20
+      t.integer :gas_used, :limit => 8, :null => false
     end
     add_index :transaction_receipts, :tx_hash, :unique => true
 
-    # Use state_blocks to store the blocks which we updated them to our accounts and contracts table
+    # Use state_blocks to record the blocks for which state (accounts and contracts table) are updated.
     create_table :state_blocks do |t|
       t.integer :number, :limit => 8, :null => false
     end
@@ -56,7 +54,7 @@ class InitializeIndexerTables < ActiveRecord::Migration
     create_table :contract_code do |t|
       t.binary :address, :limit => 20, :null => false
       t.binary :hash, :limit => 32, :null => false
-      t.text :code, :limit => 10.megabyte, :null => false
+      t.text :code, :limit => 1.megabyte, :null => false
     end
     add_index :contract_code, :address, :unique => true
 
@@ -64,7 +62,7 @@ class InitializeIndexerTables < ActiveRecord::Migration
       t.binary :address, :limit => 20, :null => false
       t.integer :block_number, :limit => 8, :null => false
       t.string :balance, :limit => 32, :null => false
-      t.integer :nonce, :limit => 8
+      t.integer :nonce, :limit => 8, :null => false
     end
     add_index :accounts, :address
     add_index :accounts, [:address, :block_number], :unique => true
