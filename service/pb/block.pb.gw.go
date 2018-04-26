961feb4739f963148cacc11e5699d8dbb7449df1
kimmylin <30611210+kimmylin@users.noreply.github.com>
2018-04-26 14:53:45 +0800

    service: update json tag to *NOT* omit empty

--- service/pb/block.pb.gw.go
+++ service/pb/block.pb.gw.go
@@ -198,9 +198,9 @@ func RegisterBlockServiceHandlerClient(ctx context.Context, mux *runtime.ServeMu
 }
 
 var (
-	pattern_BlockService_GetBlockByHash_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2}, []string{"v1", "blocks", "hash"}, ""))
+	pattern_BlockService_GetBlockByHash_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 2}, []string{"v1", "blocks", "hash"}, ""))
 
-	pattern_BlockService_GetBlockByNumber_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2}, []string{"v1", "blocks", "number"}, ""))
+	pattern_BlockService_GetBlockByNumber_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 2}, []string{"v1", "blocks", "number"}, ""))
 )
 
 var (
