a28213a71e5e44a550ab6ea2a44aff4c8d67a54e
mark.lin <mark@maicoin.com>
2018-06-11 12:22:54 +0800

    migration: add receipt logs table

--- migration/db/schema.rb
+++ migration/db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,88 +10,97 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180313051512) do
+ActiveRecord::Schema.define(version: 2018_06_08_083123) do
 
-  create_table "accounts", force: :cascade do |t|
-    t.binary  "address",      limit: 20, null: false
-    t.integer "block_number", limit: 8,  null: false
-    t.string  "balance",      limit: 32, null: false
-    t.integer "nonce",        limit: 8,  null: false
+  create_table "accounts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci", force: :cascade do |t|
+    t.binary "address", limit: 20, null: false
+    t.bigint "block_number", null: false
+    t.string "balance", limit: 32, null: false
+    t.bigint "nonce", null: false
+    t.index ["address", "block_number"], name: "index_accounts_on_address_and_block_number", unique: true
+    t.index ["address"], name: "index_accounts_on_address"
   end
 
-  add_index "accounts", ["address", "block_number"], name: "index_accounts_on_address_and_block_number", unique: true, using: :btree
-  add_index "accounts", ["address"], name: "index_accounts_on_address", using: :btree
-
-  create_table "block_headers", force: :cascade do |t|
-    t.binary  "hash",         limit: 32,   null: false
-    t.binary  "parent_hash",  limit: 32,   null: false
-    t.binary  "uncle_hash",   limit: 32,   null: false
-    t.binary  "coinbase",     limit: 20,   null: false
-    t.binary  "root",         limit: 32,   null: false
-    t.binary  "tx_hash",      limit: 32,   null: false
-    t.binary  "receipt_hash", limit: 32,   null: false
-    t.integer "difficulty",   limit: 8,    null: false
-    t.integer "number",       limit: 8,    null: false
-    t.integer "gas_limit",    limit: 8,    null: false
-    t.integer "gas_used",     limit: 8,    null: false
-    t.integer "time",         limit: 8,    null: false
-    t.binary  "extra_data",   limit: 1024
-    t.binary  "mix_digest",   limit: 32,   null: false
-    t.binary  "nonce",        limit: 8,    null: false
+  create_table "block_headers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci", force: :cascade do |t|
+    t.binary "hash", limit: 32, null: false
+    t.binary "parent_hash", limit: 32, null: false
+    t.binary "uncle_hash", limit: 32, null: false
+    t.binary "coinbase", limit: 20, null: false
+    t.binary "root", limit: 32, null: false
+    t.binary "tx_hash", limit: 32, null: false
+    t.binary "receipt_hash", limit: 32, null: false
+    t.bigint "difficulty", null: false
+    t.bigint "number", null: false
+    t.bigint "gas_limit", null: false
+    t.bigint "gas_used", null: false
+    t.bigint "time", null: false
+    t.binary "extra_data", limit: 1024
+    t.binary "mix_digest", limit: 32, null: false
+    t.binary "nonce", limit: 8, null: false
+    t.index ["hash"], name: "index_block_headers_on_hash", unique: true
+    t.index ["number"], name: "index_block_headers_on_number", unique: true
   end
 
-  add_index "block_headers", ["hash"], name: "index_block_headers_on_hash", unique: true, using: :btree
-  add_index "block_headers", ["number"], name: "index_block_headers_on_number", unique: true, using: :btree
-
-  create_table "erc20", force: :cascade do |t|
-    t.binary  "address",      limit: 20,       null: false
-    t.integer "block_number", limit: 8,        null: false
-    t.binary  "code",         limit: 16777215, null: false
-    t.string  "name",         limit: 32
-    t.string  "total_supply", limit: 32
-    t.integer "decimals",     limit: 8
+  create_table "erc20", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci", force: :cascade do |t|
+    t.binary "address", limit: 20, null: false
+    t.bigint "block_number", null: false
+    t.binary "code", limit: 16777215, null: false
+    t.string "name", limit: 32
+    t.string "total_supply", limit: 32
+    t.bigint "decimals"
+    t.index ["address"], name: "index_erc20_on_address", unique: true
+    t.index ["block_number"], name: "index_erc20_on_block_number"
   end
 
-  add_index "erc20", ["address"], name: "index_erc20_on_address", unique: true, using: :btree
-  add_index "erc20", ["block_number"], name: "index_erc20_on_block_number", using: :btree
-
-  create_table "total_difficulty", force: :cascade do |t|
-    t.integer "block", limit: 8,   null: false
-    t.binary  "hash",  limit: 32,  null: false
-    t.string  "td",    limit: 255, null: false
+  create_table "receipt_logs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci", force: :cascade do |t|
+    t.binary "tx_hash", limit: 32, null: false
+    t.bigint "block_number", null: false
+    t.binary "contract_address", limit: 20, null: false
+    t.binary "event_name", limit: 32, null: false
+    t.binary "topic1", limit: 32
+    t.binary "topic2", limit: 32
+    t.binary "topic3", limit: 32
+    t.binary "data", limit: 16777215, null: false
+    t.index ["block_number", "contract_address", "event_name"], name: "index_receipt_logs_on_nae"
+    t.index ["block_number", "contract_address"], name: "index_receipt_logs_on_block_number_and_contract_address"
+    t.index ["block_number"], name: "index_receipt_logs_on_block_number"
+    t.index ["tx_hash"], name: "index_receipt_logs_on_tx_hash"
   end
 
-  add_index "total_difficulty", ["hash"], name: "index_total_difficulty_on_hash", unique: true, using: :btree
-
-  create_table "transaction_receipts", force: :cascade do |t|
-    t.binary  "root",                limit: 32
-    t.integer "status",              limit: 1
-    t.integer "cumulative_gas_used", limit: 8,               null: false
-    t.binary  "bloom",               limit: 256,             null: false
-    t.binary  "tx_hash",             limit: 32,              null: false
-    t.binary  "contract_address",    limit: 20
-    t.integer "gas_used",            limit: 8,               null: false
-    t.integer "block_number",        limit: 8,   default: 0, null: false
+  create_table "total_difficulty", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci", force: :cascade do |t|
+    t.bigint "block", null: false
+    t.binary "hash", limit: 32, null: false
+    t.string "td", null: false
+    t.index ["hash"], name: "index_total_difficulty_on_hash", unique: true
   end
 
-  add_index "transaction_receipts", ["block_number"], name: "index_transaction_receipts_on_block_number", using: :btree
-  add_index "transaction_receipts", ["tx_hash"], name: "index_transaction_receipts_on_tx_hash", unique: true, using: :btree
-
-  create_table "transactions", force: :cascade do |t|
-    t.binary  "hash",         limit: 32,                   null: false
-    t.binary  "block_hash",   limit: 32,                   null: false
-    t.binary  "from",         limit: 20,                   null: false
-    t.binary  "to",           limit: 20
-    t.integer "nonce",        limit: 8,                    null: false
-    t.string  "gas_price",    limit: 32,                   null: false
-    t.integer "gas_limit",    limit: 8,                    null: false
-    t.string  "amount",       limit: 32,                   null: false
-    t.binary  "payload",      limit: 16777215,             null: false
-    t.integer "block_number", limit: 8,        default: 0, null: false
+  create_table "transaction_receipts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci", force: :cascade do |t|
+    t.binary "root", limit: 32
+    t.integer "status", limit: 1
+    t.bigint "cumulative_gas_used", null: false
+    t.binary "bloom", limit: 256, null: false
+    t.binary "tx_hash", limit: 32, null: false
+    t.binary "contract_address", limit: 20
+    t.bigint "gas_used", null: false
+    t.bigint "block_number", null: false
+    t.index ["block_number"], name: "index_transaction_receipts_on_block_number"
+    t.index ["tx_hash"], name: "index_transaction_receipts_on_tx_hash", unique: true
   end
 
-  add_index "transactions", ["block_hash"], name: "index_transactions_on_block_hash", using: :btree
-  add_index "transactions", ["block_number"], name: "index_transactions_on_block_number", using: :btree
-  add_index "transactions", ["hash"], name: "index_transactions_on_hash", unique: true, using: :btree
+  create_table "transactions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci", force: :cascade do |t|
+    t.binary "hash", limit: 32, null: false
+    t.binary "block_hash", limit: 32, null: false
+    t.binary "from", limit: 20, null: false
+    t.binary "to", limit: 20
+    t.bigint "nonce", null: false
+    t.string "gas_price", limit: 32, null: false
+    t.bigint "gas_limit", null: false
+    t.string "amount", limit: 32, null: false
+    t.binary "payload", limit: 16777215, null: false
+    t.bigint "block_number", null: false
+    t.index ["block_hash"], name: "index_transactions_on_block_hash"
+    t.index ["block_number"], name: "index_transactions_on_block_number"
+    t.index ["hash"], name: "index_transactions_on_hash", unique: true
+  end
 
 end
